09d5eddcf3855f3c2891697715b72a6aa0a85312
Lucas Siq <lucasdassiqueira@gmail.com>
2022-10-22 03:09:29 -0300

    twelfth Commit

--- app/src/main/java/br/com/horta_viva/loja.kt
+++ app/src/main/java/br/com/horta_viva/loja.kt
@@ -1,12 +1,22 @@
 package br.com.horta_viva
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class loja : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide();
         setContentView(R.layout.activity_loja)
+
+        val back: Button = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.back)
+
+        back.setOnClickListener{
+            val i = Intent(this, home::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
